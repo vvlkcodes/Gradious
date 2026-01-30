f7c19522c5e7182f2dbcce4b38da79a3c000968a
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2026-01-30 14:09:14 +0530

    Create ThreeSum.java

---
+++ 30-1-2026/ThreeSum.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> result = sol.threeSum(nums);
+
+        System.out.println(result);
+    }
+}
+
+class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+
+        // Step 2: Fix one element and use two pointers
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    // Skip duplicate values for j and k
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+                else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
+
